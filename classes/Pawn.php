87057c627fc7f11482fd7e82cb11cc05f48b3133
gnieark <gnieark@free.fr>
2025-02-10 22:00:25 +0100

    knight

--- classes/Pawn.php
+++ classes/Pawn.php
@@ -1,5 +1,31 @@
 <?php
 class Pawn extends Piece{
-    
+
+    public function get_available_destinations($current): array {
+        $mvts = array();
+        if( $this -> isWhite() ){
+            if(($current > 7) && ($current <16)){
+                //the pawn is on the second row
+                $mvt = new Movement();
+                $mvt-> set_origin($current)
+                    ->add_cell_to_path($current + 8)
+                    ->set_dest($current + 16 )
+                    ->set_destCanBeAnOpponent(false);
+                $mvts[] = $mvt;
+            }
+            if( $current < 56 ){
+                $mvt = new Movement();
+                $mvt->set_origin($current)
+                    ->set_dest($current + 8 )
+                    ->set_destCanBeAnOpponent(false);
+            }
+
+
+        }else{
+
+
+        }
+        return $mvts;
+    }
 
 }
